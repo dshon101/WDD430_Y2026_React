d2e0094e179ae9fa955a5b8768fb084dcff8c374
dshon101 <munashedemetrious@gmail.com>
2026-07-03 15:10:32 -0600

    Build todo list with add, toggle, delete, and localStorage

---
+++ src/TodoItem.jsx
@@ -0,0 +1,15 @@
+function TodoItem({ todo, toggleTodo, deleteTodo }) {
+  return (
+    <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleTodo(todo.id)}
+      />
+      <span>{todo.title}</span>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    </li>
+  )
+}
+
+export default TodoItem
